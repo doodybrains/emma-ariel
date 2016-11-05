a06f61be46e2d63b5488a203f2ec1a996b924f3e
doodybrains <doodybrainz@gmail.com>
2016-11-05 19:06:00 -0400

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 require('./app.scss');
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>New App With Sass</h2>
+      <div className="container">
+        <div className="emma">
+          <h2>websites</h2>
+          <a href="#" target="_blank">website with the sound of its own making</a>
+          <a href="#" target="_blank">know thyself</a>
+          <a href="#" target="_blank">erica eso</a>
+        </div>
+        <div className="ariel">
+          <h2>poetry</h2>
+          <a href="#" target="_blank">link</a>
+          <a href="#" target="_blank">link</a>
+          <a href="#" target="_blank">link</a>
         </div>
       </div>
     );
