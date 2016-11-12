0c6e594cea1091b84510ee32e58d3f9a58ab182e
doodybrains <doodybrainz@gmail.com>
2016-11-12 17:22:24 -0500

    Update links

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ class App extends Component {
     return (
       <div className="container">
         <div className="emma">
-          <h2>websites</h2>
-          <a href="#" target="_blank">website with the sound of its own making</a>
-          <a href="#" target="_blank">know thyself</a>
-          <a href="#" target="_blank">erica eso</a>
+          <h2>web projects</h2>
+          <a href="http://emmaraeworks.com/wwtsoiom/" target="_blank">website with the sound of its own making</a>
+          <a href="https://doodybrains.github.io/knowthyself/" target="_blank">know thyself</a>
+          <a href="http://ericaeso.com/" target="_blank">erica eso</a>
         </div>
         <div className="ariel">
-          <h2>poetry</h2>
-          <a href="#" target="_blank">link</a>
-          <a href="#" target="_blank">link</a>
-          <a href="#" target="_blank">link</a>
+          <h2>poetry + art projects</h2>
+          <a href="http://www.twoseriousladies.org/two-poems-by-ariel-yelen/" target="_blank">Two Serious Ladies (2 poems)</a>
+          <a href="http://jdbrecords.blogspot.com/2015/04/a-poem-by-ariel-yelen_21.html" target="_blank">Sometimes I Am Like the Flower of Farewell</a>
+          <a href="http://www.peninsulaartspace.com/shifters/" target="_blank">Shifters @ Peninsula Art Space</a>
         </div>
       </div>
     );
